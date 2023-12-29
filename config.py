c5eb81acdd2b3d92aaf87c9500189974f00a619f
@SUMIT_KING <150422862+6354882957vishal@users.noreply.github.com>
2023-12-29 09:18:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "9a2fce85339bb79254a55368a61ab92f")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6793109224:AAGR1QHXYgvkZRLhbyWbheTmA5EhHPlIv30") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "SUMIT_KING_2") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "renamev1994_bot") 
 
              # Don't Remove Credit @SUMIT_KING2
              # Subscribe YouTube Channel For Amazing Bot @SUMIT_KING9
